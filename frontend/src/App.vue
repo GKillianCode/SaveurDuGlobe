5d12adffdb7b0bb3d64390c5bf9e7ff76856c423
Killian Godet <godetkillian@yahoo.com>
2023-06-27 16:15:38 +0200

    PPJ-99

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,42 @@
 
 <script>
-import "../src/styles/main.css";
+import "./styles/main.css";
+import mobileNavigationComponent from './components/mobileNavigationComponent.vue';
+import desktopNavigationComponentVue from './components/desktopNavigationComponent.vue';
+
 export default {
+  components: {
+    mobileNavigationComponent,
+    desktopNavigationComponentVue
+  },
+  data() {
+    return {
+      screenWidth: null
+    };
+  },
+  mounted() {
+    this.screenWidth = window.innerWidth;
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.screenWidth = window.innerWidth;
+    }
+  }
 }
 </script>
 
 <template>
+  <div v-if="screenWidth < 1000">
+    <mobileNavigationComponent />
+  </div>
+  <div v-else>
+    <desktopNavigationComponentVue />
+  </div>
   <RouterView />
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss" scoped></style>
